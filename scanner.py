c738db60034178c0547d678b61901db5adf1dda6
Juergen Hardt <hardt.juergen.mail@web.de>
2025-08-02 18:58:13 +0200

    Docstrings added

--- scanner.py
+++ scanner.py
@@ -2,6 +2,8 @@ from cmd import Cmd
 import sys
 import re
 from library.classes.tracking_factory import TrackingFactory
+
+
 class CMDScanner(Cmd):
     prompt = 'DHL Parcel Scanner >> '
     welcome_message = "Welcome to DHL Parcel Scanner."
@@ -12,6 +14,9 @@ class CMDScanner(Cmd):
     intro = f'{welcome_message}\n{subtitle}\n{help_message}\n{separator}\n'
 
     def do_track(self, line):
+        """ Main function to track a parcel
+        usage: track <tracking number> (--history) """
+        
         try:
             if isinstance(line, str):
                 tracking_numbers = []
@@ -23,17 +28,20 @@ class CMDScanner(Cmd):
                 tracking_numbers = re.split(' ', line)
                 tracking_numbers.pop()
 
-
             o_tracking_factory = TrackingFactory(tracking_numbers)
-            concrete_tracker = o_tracking_factory.create_tracker(history_option)
+            concrete_tracker = o_tracking_factory.create_tracker(
+                history_option)
             concrete_tracker.run(tracking_numbers)
         except IndexError:
             print("Error - no tracking number provided")
 
-
     def do_exit(self, line):
+        """ Exits the programm """
+
         input("Thank you for using DHLPyScan - Press ENTER to quit...")
         sys.exit()
-    
+
     def do_quit(self, line):
+        """ Exits the programm """
+
         self.do_exit(line)
