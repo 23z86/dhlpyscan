e1ddf689b2769eda707351896d1134cfbc7922e4
Juergen Hardt <hardt.juergen.mail@web.de>
2025-08-02 17:15:13 +0200

    timeout parameter in requests.get() added

--- library/classes/single_tracker.py
+++ library/classes/single_tracker.py
@@ -13,7 +13,7 @@ class SingleTracker(ITracker):
         single_tracking_number = tracking_number[0]
         url = "https://www.dhl.de/int-verfolgen/data/search/?piececode=" + \
             single_tracking_number + "&language=de" + "&cid=pulltorefresh"
-        json_raw_data = requests.get(url).json()
+        json_raw_data = requests.get(url, timeout=None).json()
 
         if "keineDatenVerfuegbar" in json_raw_data['sendungen'][0]['sendungNichtGefunden']:
             print(f"Sendung {single_tracking_number} nicht gefunden")
